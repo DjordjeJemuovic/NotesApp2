ad0a4c4a28873977100cc33e71251aa3257d82d6
DjordjeJemuovic <72411508+DjordjeJemuovic@users.noreply.github.com>
2023-08-09 15:56:13 +0200

    op

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import "./App.css";
 import Notes from "./pages/Notes";
 import CreateNote from "./pages/CreateNote";
 import EditNote from "./pages/EditNote";
-import dummyNotes from './dummy-notes';
+// import dummyNotes from './dummy-notes';
 
 import { useState } from "react";
 
 function App() {
-  const [notes,setNotes] = useState(dummyNotes);
+  const [notes,setNotes] = useState([]);
   return (
     <div className="App">
       <main id="app">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Notes notes={notes} />} />
-            <Route path="/create-note" element={<CreateNote />} />
+            <Route path="/create-note" element={<CreateNote  setNotes={setNotes} />} 
+            setNotes={setNotes} />
             <Route path="/edit-note" element={<EditNote />} />
           </Routes>
         </BrowserRouter>
--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,47 +1,61 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import {IoIosArrowBack} from 'react-icons/io';
-import {v4 as uuid} from 'uuid';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
+import { v4 as uuid } from "uuid";
 
-import useCreateDate from '../components/useCreateDate';
+import useCreateDate from "../components/useCreateDate";
 
+const CreateNote = ({ setNotes }) => {
+  const [title, setTitle] = useState("");
+  const [details, setDeteils] = useState("");
+  const date = useCreateDate();
+  const navigate = useNavigate();
 
-const CreateNote = () =>{
-    const [title,setTitle] = useState('');
-    const [details,setDeteils] = useState('');
-    const date = useCreateDate();
-
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        if(title && details)
-        {
-             const note = {
-                id:uuid(),
-                title,
-                details,
-                date
-             }
-
-             console.log(note);
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && details) {
+      const note = {
+        id: uuid(),
+        title,
+        details,
+        date,
+      };
       
-    };
+      setNotes(prevNotes => [note, ...prevNotes]);
 
+      navigate("/");
+    }
+  };
 
-return(
-   <div>
-    <section>
+  return (
+    <div>
+      <section>
         <header className="create-note__header">
-            <Link to='/' className='btn'><IoIosArrowBack/></Link>
-            <button className='btn lg primary' onClick={handleSubmit}>Save</button>
+          <Link to="/" className="btn">
+            <IoIosArrowBack />
+          </Link>
+          <button className="btn lg primary" onClick={handleSubmit}>
+            Save
+          </button>
         </header>
-        <form  className="create-note__form" onSubmit={handleSubmit}>
-            <input type="text" placeholder='Title' value={title} onChange={(e)=>setTitle(e.target.value)} autoFocus/>
-            <textarea  rows="28" placeholder='Note details...' value={details} onChange={(e) => setDeteils(e.target.value)}></textarea>
+        <form className="create-note__form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            autoFocus
+          />
+          <textarea
+            rows="28"
+            placeholder="Note details..."
+            value={details}
+            onChange={(e) => setDeteils(e.target.value)}
+          ></textarea>
         </form>
-    </section>
+      </section>
     </div>
-);
-}
+  );
+};
 
-export default CreateNote ;
+export default CreateNote;
